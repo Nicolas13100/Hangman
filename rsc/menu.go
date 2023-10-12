a94f25dd50d66dcaea5ac0575c8446c07588989e
Nicolas <nicolas.moyon@ynov.com>
2023-10-12 10:28:09 +0200

    update again, Hangmans appear

--- rsc/menu.go
+++ rsc/menu.go
@@ -8,7 +8,9 @@ func Menu() {
 		var choice int
 		switch choice {
 		case 1:
+			PenduAlloween()
 		case 2:
+			Pendu()
 		default:
 			fmt.Println("Choix invalide, Seul 1 ou 2 sont accepté")
 			continue

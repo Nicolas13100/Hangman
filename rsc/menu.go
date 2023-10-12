185b0f9d6eddf888c2666d04f20bf716486a9092
Nicolas <nicolas.moyon@ynov.com>
2023-10-12 09:53:45 +0200

    Let's gooo

--- rsc/menu.go
+++ rsc/menu.go
@@ -1,5 +1,17 @@
 package Hangman
 
-func Menu() {
+import "fmt"
 
+func Menu() {
+	fmt.Println("Souhaitez vous faire 1.Pendu d'alloween ou 2.Pendu classic ?")
+	for {
+		var choice int
+		switch choice {
+		case 1:
+		case 2:
+		default:
+			fmt.Println("Choix invalide, Seul 1 ou 2 sont accepté")
+			continue
+		}
+	}
 }
